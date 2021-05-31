6444372be58e36a56569524a5a179920fb98a0b6
acmesun <danfyou@gmail.com>
2021-05-31 16:18:46 +0300

    added log4j configuration

--- src/main/java/by/lukyanets/multithreading2/entity/Port.java
+++ src/main/java/by/lukyanets/multithreading2/entity/Port.java
@@ -1,13 +1,18 @@
 package by.lukyanets.multithreading2.entity;
 
+import by.lukyanets.multithreading2.exception.ThreadException;
 import by.lukyanets.multithreading2.reader.DataReader;
 import by.lukyanets.multithreading2.util.Util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 public class Port {
     private static final Port INSTANCE = new Port();
+    private final Logger logger = LogManager.getLogger(Port.class);
     private final Semaphore piers;
     private final Semaphore portCapacity;
     private final Semaphore availableContainers;
@@ -25,6 +30,39 @@ public class Port {
     public static Port getINSTANCE() {
         return INSTANCE;
     }
+
+    public void arrive() throws ThreadException {
+        try {
+            logger.info("Ship {} arrived.", Thread.currentThread().getName());
+            piers.acquire();
+        } catch (InterruptedException e) {
+            logger.error("There is an error in arrival.");
+            throw new ThreadException();
+        }
+    }
+
+    public void depart() {
+        logger.info("Ship {} departed", Thread.currentThread().getName());
+        piers.release();
+    }
+
+    public void unload(int containersNumber) throws InterruptedException {
+        logger.info("Ship {}  unloaded with {}", Thread.currentThread().getName(), containersNumber);
+        TimeUnit.SECONDS.sleep(3);
+        portCapacity.release(containersNumber);
+        logger.info("Warehouse is {}", portCapacity.availablePermits());
+    }
+
+    public void load(int containersNumber) throws InterruptedException {
+        if (portCapacity.tryAcquire(containersNumber, 1000, TimeUnit.MILLISECONDS)) {
+            logger.info("Ship {}  unloaded with {}", Thread.currentThread().getName(), containersNumber);
+            logger.info("Warehouse is {}", portCapacity.availablePermits());
+            TimeUnit.SECONDS.sleep(3);
+        } else {
+            logger.warn("Ship {} cannot be loaded, leaving.", Thread.currentThread().getName());
+        }
+    }
+
 
     @Override
     public String toString() {
--- src/main/java/by/lukyanets/multithreading2/entity/Ship.java
+++ src/main/java/by/lukyanets/multithreading2/entity/Ship.java
@@ -1,6 +1,10 @@
 package by.lukyanets.multithreading2.entity;
 
+import by.lukyanets.multithreading2.exception.ThreadException;
+
 public class Ship extends Thread {
+    private final Port port = Port.getINSTANCE();
+
     private Integer onBoard;
     private Integer size;
 
@@ -8,6 +12,31 @@ public class Ship extends Thread {
         this.onBoard = onBoard;
         this.size = size;
     }
+
+    @Override
+    public void run() {
+        try {
+            port.arrive();
+            if (this.getOnBoard() > 0) {
+                port.unload(this.getOnBoard());
+            } else {
+                port.load(this.getSize());
+            }
+        } catch (ThreadException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            port.depart();
+        }
+    }
+
+    public Integer getOnBoard() {
+        return onBoard;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
     @Override
     public String toString() {
         return "Ship{" +
